581e5a1df9369bc11556d528d97daf11a7b86e65
creativekdev <creativekdev@gmail.com>
2024-09-11 12:51:53 -0600

    updated design

--- main.js
+++ main.js
@@ -2,27 +2,30 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow () {
-  const mainWindow = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
-      contextIsolation: false,
+      contextIsolation: false
     }
   });
 
-  mainWindow.loadFile('index.html');
+  // Make sure the path to index.html is correct
+  win.loadFile('src/index.html');
 }
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
